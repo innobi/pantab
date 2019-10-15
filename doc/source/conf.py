18b999e421541debe9603ec4cede8fb58e352ddb
Will Ayd <william.ayd@icloud.com>
2019-10-14 18:13:30 -0700

    Simple usage scenario

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -4,6 +4,8 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sphinx_rtd_theme
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -33,6 +35,7 @@ release = '0.0.1'
 extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,11 +54,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_sidebars = { '**': ['globaltoc.html'] }
--- doc/source/examples.rst
+++ doc/source/examples.rst
@@ -1,5 +1,32 @@
-Examples
-========
+Usage Examples
+==============
 
-.. todo::
-   Document usage examples
+Writing to a Hyper Extract
+--------------------------
+
+.. code-block:: python
+
+   import pandas as pd
+   import pantab
+
+   df = pd.DataFrame([
+       ["dog", 4],
+       ["cat", 4],
+   ], columns=["animal", "num_of_legs"])
+
+   pantab.frame_to_hyper(df, "example.hyper", "animals")
+
+The above example will write out to a file named "example.hyper", which Tableau can then report off of.
+
+.. image:: _static/simple_write.png
+
+Reading a Hyper Extract
+-----------------------
+
+.. code-block:: python
+
+   import pantab
+
+   df = pantab.frame_from_hyper("example.hyper", "animals")
+   print(df)
+
