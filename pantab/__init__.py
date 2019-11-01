d02c12cd5b3d2fd870598319e5f3344734d320ff
Will Ayd <william.ayd@icloud.com>
2019-11-01 09:06:04 -0700

    Make linters play nice

--- pantab/__init__.py
+++ pantab/__init__.py
@@ -1,6 +1,6 @@
 from ._pantab import (
     frame_from_hyper,
-    frames_from_hyper,
     frame_to_hyper,
+    frames_from_hyper,
     frames_to_hyper,
 )
--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
-from setuptools import setup, find_packages
 from os import path
+
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='pantab',
-    version='0.0.1.b1',
-    description='Converts pandas DataFrames into Tableau Hyper Extracts',
+    name="pantab",
+    version="0.0.1.b1",
+    description="Converts pandas DataFrames into Tableau Hyper Extracts",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/WillAyd/pantab',
-    author='Will Ayd',
-    author_email='william.ayd@icloud.com',
-    license='BSD',
+    long_description_content_type="text/markdown",
+    url="https://github.com/WillAyd/pantab",
+    author="Will Ayd",
+    author_email="william.ayd@icloud.com",
+    license="BSD",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Office/Business',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Office/Business",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='tableau visualization pandas dataframe',
-    packages=find_packages(exclude=['samples', 'tests']),
-    python_requires='>=3.6',
-    install_requires=['pandas'],
-    extras_require={
-        'dev': ['pytest'],
-    },
+    keywords="tableau visualization pandas dataframe",
+    packages=find_packages(exclude=["samples", "tests"]),
+    python_requires=">=3.6",
+    install_requires=["pandas"],
+    extras_require={"dev": ["pytest"]},
 )
