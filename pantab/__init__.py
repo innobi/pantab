7c8e6dad027679429e924eb43e3bf02929a8dbda
Will Ayd <william.ayd@icloud.com>
2019-11-22 09:55:06 -0800

    big split

--- pantab/__init__.py
+++ pantab/__init__.py
@@ -1,9 +1,7 @@
 __version__ = "0.0.1.b5"
 
 
-from ._pantab import (
-    frame_from_hyper,
-    frame_to_hyper,
-    frames_from_hyper,
-    frames_to_hyper,
-)
+from ._reader import frame_from_hyper, frames_from_hyper
+from ._writer import frame_to_hyper, frames_to_hyper
+
+__all__ = ["__version__", "frame_from_hyper", "frames_from_hyper", "frame_to_hyper", "frames_to_hyper"]
