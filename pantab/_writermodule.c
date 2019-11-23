f6bb56cd149389c6da2a82f62f4dcc81e9db8fdc
Will Ayd <william.ayd@icloud.com>
2019-11-22 17:23:15 -0800

    iteration in order

--- pantab/_writermodule.c
+++ pantab/_writermodule.c
@@ -9,6 +9,9 @@ static PyObject *write_to_hyper(PyObject *dummy, PyObject *args) {
   int ok;
   PyArrayObject *array;
   PyObject *funcList;
+  NpyIter* iter;
+  NpyIter_IterNextFunc *iternext;
+  int nonzero_count = 0;
   
   ok = PyArg_ParseTuple(args, "O!O!", &PyArray_Type, &array, &PyList_Type, &funcList);
 
@@ -25,7 +28,30 @@ static PyObject *write_to_hyper(PyObject *dummy, PyObject *args) {
     PyErr_SetString(PyExc_ValueError, "Number of columns in supplied data must match value list");
     return NULL;
   }
-    
+
+  // Do nothing for unsized arrays
+  if (PyArray_SIZE(array) == 0) {
+    return Py_None;
+  }
+
+  iter = NpyIter_New(array, NPY_ITER_READONLY, NPY_KEEPORDER, NPY_NO_CASTING, NULL);
+  if (iter == NULL)
+    return NULL;
+
+  iternext = NpyIter_GetIterNext(iter, NULL);
+  if (iternext == NULL) {
+    NpyIter_Deallocate(iter);
+    return NULL;
+  }
+
+  do {
+    nonzero_count += 1;
+    /* Increment the iterator to the next inner loop */
+  } while(iternext(iter));
+
+   NpyIter_Deallocate(iter);
+
+   printf("I have %d elements\n", nonzero_count);
   
   return Py_None;
 }
