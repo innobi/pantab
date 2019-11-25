c6dc186c04b9c0ef1d4c25aab1cad520647067cd
Will Ayd <william.ayd@icloud.com>
2019-11-24 22:49:48 -0800

    revert

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 from os import path
 import sys
 
@@ -11,12 +10,10 @@ dll_path = find_hyper_api_dll()
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-
-os.environ['LD_LIBRARY_PATH'] = str(dll_path.parent.resolve())
-
 writer_module = Extension(
     "libwriter",
     sources=["pantab/_writermodule.c"],
+    library_dirs=[str(dll_path.parent.resolve())],
     libraries=[dll_path.stem.replace("lib", "")],
 )
 
