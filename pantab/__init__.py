4898823c49fc0937ba13e098b58c91a94ad9a900
Will Ayd <william.ayd@icloud.com>
2020-02-03 17:37:03 -0800

    version bump

--- pantab/__init__.py
+++ pantab/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 from ._reader import frame_from_hyper, frames_from_hyper
--- pantab/newsfragments/84.feature
+++
@@ -1,1 +0,0 @@
-Pre-built wheels for manylinux configurations are now available.
