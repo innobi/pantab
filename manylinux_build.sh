b7b7c2808a61e3fb16799a9814fd0f251fd5cc32
Will Ayd <william.ayd@icloud.com>
2020-02-05 11:33:31 -0800

    script updates

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -16,8 +16,8 @@ PYLOC=/opt/python/cp${PYVER2}-cp${PYVER2}${ABIVER}
 # Clear any existing pip caches
 which ${PYLOC}/bin/python
 ${PYLOC}/bin/python -m pip install --upgrade pip
-${PYLOC}/bin/python -m pip install --upgrade setuptools wheel
-${PYLOC}/bin/python -m pip install auditwheel pandas tableauhyperapi
+${PYLOC}/bin/python -m pip install --upgrade setuptools wheel auditwheel
+${PYLOC}/bin/python -m pip install tableauhyperapi
 
 # Hack so auditwheel can find libtableauhyperapi
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PYLOC}/lib/python${PYVER}/site-packages/tableauhyperapi/bin
