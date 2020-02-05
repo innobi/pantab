c53998d578ca371a758627ef6ea3d2576305f42a
Will Ayd <william.ayd@icloud.com>
2020-02-05 10:44:54 -0800

    Added imperative test program

--- pantab/__init__.py
+++ pantab/__init__.py
@@ -3,6 +3,7 @@ __version__ = "1.0.1"
 
 from ._reader import frame_from_hyper, frames_from_hyper
 from ._writer import frame_to_hyper, frames_to_hyper
+from ._tester import test
 
 __all__ = [
     "__version__",
@@ -10,4 +11,5 @@ __all__ = [
     "frames_from_hyper",
     "frame_to_hyper",
     "frames_to_hyper",
+    "test",
 ]
---
+++ pantab/_tester.py
@@ -0,0 +1,11 @@
+import pathlib
+import sys
+
+
+PKG = pathlib.Path(__file__).resolve().parent
+
+
+def test():
+    import pytest
+    sys.exit(pytest.main([str(PKG)]))
+    
