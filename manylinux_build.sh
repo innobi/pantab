90fd63bbf42203f27cd65c762a501da109296fc8
Will Ayd <william.ayd@icloud.com>
2020-02-04 17:31:24 -0800

    progress

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
-/opt/python/${PYVER}/bin/pip install --upgrade pip
-/opt/python/${PYVER}/bin/pip install install setuptools wheel pandas tableauhyperapi
-/opt/python/${PYVER}/bin/pip wheel /io/ -w wheelhouse/
+/opt/python/cp${PYVER}-cp${PYVER}m/bin/pip install --upgrade pip
+/opt/python/cp${PYVER}-cp${PYVER}m/bin/pip install --upgrade setuptools
+/opt/python/cp${PYVER}-cp${PYVER}m/bin/pip install wheel pandas tableauhyperapi
+
+cd /io
+/opt/python/cp${PYVER}-cp${PYVER}m/bin/python setup.py bdist_wheel
+for whl in dist/*.whl; do
+    /opt/python/cp${PYVER}-cp${PYVER}m/bin/python -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+
+
