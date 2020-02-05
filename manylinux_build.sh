86972776264236ddd0e6af49eeb8d95f6100c737
Will Ayd <william.ayd@icloud.com>
2020-02-05 09:09:20 -0800

    Updated workflow

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -1,14 +1,33 @@
 #!/bin/sh -l
 
-/opt/python/cp${PYVER}-cp${PYVER}m/bin/pip install --upgrade pip
-/opt/python/cp${PYVER}-cp${PYVER}m/bin/pip install --upgrade setuptools
-/opt/python/cp${PYVER}-cp${PYVER}m/bin/pip install wheel pandas tableauhyperapi
+# PYVER2 removes the period from the version
+PYVER2="${PYVER//.}"
+
+# Starting in Python38 the ABI version is no longer required
+if [ "$PYVER2" = "37" ] || [ "$PYVER2" = "36" ]
+then
+    ABIVER="m"
+else
+    ABIVER=""
+fi
+
+PYLOC=/opt/python/cp${PYVER2}-cp${PYVER2}${ABIVER}
+
+${PYLOC}/bin/python -m pip install --upgrade pip
+${PYLOC}/bin/python -m pip install --upgrade setuptools
+${PYLOC}/bin/python -m pip install wheel pandas tableauhyperapi pytest
+
+# Hack so auditwheel can find libtableauhyperapi
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PYLOC}/lib/python${PYVER}/site-packages/tableauhyperapi/bin
 
 cd /io
-/opt/python/cp${PYVER}-cp${PYVER}m/bin/python setup.py bdist_wheel
-for whl in dist/*.whl; do
-    /opt/python/cp${PYVER}-cp${PYVER}m/bin/python -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+${PYLOC}/bin/python setup.py bdist_wheel
+# TODO: we probably only need to repair one wheel file
+for whl in dist/pantab*.whl; do
+    ${PYLOC}/bin/python -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
-
-
+# Run the tests using the installed wheel
+${PYLOC}/bin/python -m pip install pantab --no-index -f /io/wheelhouse
+cd "$HOME"
+${PYLOC}/bin/python -m pytest /io/pantab
