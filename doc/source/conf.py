78646945751b337f74c4c5927567ff4448f6e94b
William Ayd <william.ayd@icloud.com>
2021-07-02 11:52:17 -0700

    fixups for 1.3 pandas release

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,11 +1,10 @@
 from typing import List
-
 # -- Project information -----------------------------------------------------
 
-project = "pantab"
-copyright = "2019-2020, Will Ayd, innobi, LLC"
-author = "Will Ayd, innobi, LLC"
-release = "2.0.0"
+project = 'pantab'
+copyright = '2019-2020, Will Ayd, innobi, LLC'
+author = 'Will Ayd, innobi, LLC'
+release = '2.0.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -14,16 +13,16 @@ extensions = [
     "sphinx_rtd_theme",
 ]
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 master_doc = "index"  # see RTD GH issue 2569
 html_context = {
     "display_github": True,
 }
 
-html_static_path = ["_static"]
+html_static_path = ['_static']
