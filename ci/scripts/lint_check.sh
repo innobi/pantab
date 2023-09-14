c985bbe6b464a427f9f308b1031611d42bd07736
William Ayd <will_ayd@innobi.io>
2023-09-14 17:06:21 -0400

    upgrade clang-format (#193)

--- ci/scripts/lint_check.sh
+++ ci/scripts/lint_check.sh
@@ -4,4 +4,4 @@ set -e
 
 black --check /pantab 
 mypy /pantab
-clang-format-11 -i --style=LLVM --dry-run -Werror /pantab/pantab/src/*
+clang-format-15 -i --style=LLVM --dry-run -Werror /pantab/pantab/src/*
