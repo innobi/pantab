f3067bb54ab5c759c95314023a679c3f73267da2
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-11-30 21:37:05 -0600

    chore: formatting

--- pantab/_compat.py
+++ pantab/_compat.py
@@ -1,6 +1,5 @@
-from pandas.util.version import parse
-
 import pandas as pd
+from pandas.util.version import parse
 
 PANDAS_120 = parse(pd.__version__) >= parse("1.2.0")
 PANDAS_130 = parse(pd.__version__) >= parse("1.3.0")
