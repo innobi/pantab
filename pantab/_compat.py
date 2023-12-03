fe7f53684adedcfa02da0e170d62e0d35eea9281
jorwoods <jorwoods@users.noreply.github.com>
2023-12-03 14:13:03 -0600

    Jorwoods/cibuildwheel (#204)

--- pantab/_compat.py
+++ pantab/_compat.py
@@ -1,8 +1,7 @@
-from distutils.version import LooseVersion
-
 import pandas as pd
+from pandas.util.version import parse
 
-PANDAS_120 = LooseVersion(pd.__version__) >= LooseVersion("1.2.0")
-PANDAS_130 = LooseVersion(pd.__version__) >= LooseVersion("1.3.0")
+PANDAS_120 = parse(pd.__version__) >= parse("1.2.0")
+PANDAS_130 = parse(pd.__version__) >= parse("1.3.0")
 
 __all__ = ["PANDAS_120", "PANDAS_130"]
