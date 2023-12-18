278e98b36b7b83eedd432deb8e2c76e3afccc68b
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-12-18 15:22:02 -0600

    style: pre-commit

--- pantab/tests/test_types.py
+++ pantab/tests/test_types.py
@@ -3,7 +3,10 @@ import tableauhyperapi as tab_api
 
 import pantab._types
 
-@pytest.mark.parametrize("nullability", [tab_api.Nullability.NULLABLE, tab_api.Nullability.NOT_NULLABLE])
+
+@pytest.mark.parametrize(
+    "nullability", [tab_api.Nullability.NULLABLE, tab_api.Nullability.NOT_NULLABLE]
+)
 def test_read_varchar_type(nullability):
     """
     Test that we can read a VARCHAR column from Hyper.
@@ -11,4 +14,3 @@ def test_read_varchar_type(nullability):
     vchar = tab_api.SqlType.varchar(255)
     vchar_column = pantab._types._ColumnType(vchar, nullability)
     assert pantab._types._get_pandas_type(vchar_column) == "string"
-
