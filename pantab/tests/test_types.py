fa34332112f78e9b8ad2eeff75ba1d7cb32fc5e0
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-12-18 15:05:27 -0600

    test: parameterize VARCHAR type return

--- pantab/tests/test_types.py
+++ pantab/tests/test_types.py
@@ -1,21 +1,14 @@
+import pytest
 import tableauhyperapi as tab_api
 
 import pantab._types
 
-
-def test_read_varchar_type():
+@pytest.mark.parametrize("nullability", [tab_api.Nullability.NULLABLE, tab_api.Nullability.NOT_NULLABLE])
+def test_read_varchar_type(nullability):
     """
     Test that we can read a VARCHAR column from Hyper.
     """
     vchar = tab_api.SqlType.varchar(255)
-    vchar_column = pantab._types._ColumnType(vchar, tab_api.Nullability.NULLABLE)
+    vchar_column = pantab._types._ColumnType(vchar, nullability)
     assert pantab._types._get_pandas_type(vchar_column) == "string"
 
-
-def test_read_varchar_type_non_null():
-    """
-    Test that we can read a VARCHAR column from Hyper that is non nullable.
-    """
-    vchar = tab_api.SqlType.varchar(255)
-    vchar_column = pantab._types._ColumnType(vchar, tab_api.Nullability.NOT_NULLABLE)
-    assert pantab._types._get_pandas_type(vchar_column) == "string"
