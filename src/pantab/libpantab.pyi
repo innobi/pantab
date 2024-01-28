1c820fd008b1ba66b3e6154229a9b637576a035e
William Ayd <will_ayd@innobi.io>
2024-01-28 14:21:43 -0500

    mypy fixes (#252)

---
+++ src/pantab/libpantab.pyi
@@ -0,0 +1,10 @@
+from typing import Any, Literal
+
+def write_to_hyper(
+    dict_of_capsules: dict[tuple[str, str], Any],
+    path: str,
+    table_mode: Literal["w", "a"],
+    json_columns: set[str],
+    geo_columns: set[str],
+) -> None: ...
+def read_from_hyper_query(path: str, query: str): ...
