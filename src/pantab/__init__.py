d9e5e7b39f5640062ed5a2ccf3058d4d497475fe
Will Ayd <william.ayd@icloud.com>
2024-01-28 11:33:28 -0500

    new test layout

--- src/pantab/__init__.py
+++ src/pantab/__init__.py
@@ -2,7 +2,6 @@ __version__ = "4.0.0rc"
 
 
 from ._reader import frame_from_hyper, frame_from_hyper_query, frames_from_hyper
-from ._tester import test
 from ._writer import frame_to_hyper, frames_to_hyper
 
 __all__ = [
@@ -12,5 +11,4 @@ __all__ = [
     "frames_from_hyper",
     "frame_to_hyper",
     "frames_to_hyper",
-    "test",
 ]
