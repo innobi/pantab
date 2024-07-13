acd97e70564101805647e93a3b3ba5c3e376018d
Will Ayd <william.ayd@icloud.com>
2024-07-13 00:03:37 -0400

    Try COMMAND

--- src/pantab/PostInstall.cmake
+++ src/pantab/PostInstall.cmake
@@ -1,4 +1,5 @@
 # https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
 if(WIN32)
-  execute_process("mt.exe -manifest libpantab.dll.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")
+  execute_process(
+    COMMAND "mt.exe -manifest libpantab.dll.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")
 endif()
