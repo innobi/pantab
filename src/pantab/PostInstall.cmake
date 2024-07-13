2182a7a6e532ca6e6129c302b8341c9683c761af
Will Ayd <william.ayd@icloud.com>
2024-07-13 18:32:25 -0400

    More edit attempts

--- src/pantab/PostInstall.cmake
+++ src/pantab/PostInstall.cmake
@@ -1,7 +1,9 @@
 # https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
 if(WIN32)
+  # Required so local installation of pantab can resolve to bundled
+  # tableauhyperapi.dll and not any other on system
+  # https://devblogs.microsoft.com/oldnewthing/20171011-00/?p=97195
+  install(FILES libpantab.manifest DESTINATION ${SKBUILD_PROJECT_NAME}/)
   execute_process(
-    COMMAND "mt.exe -manifest tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\tableauhyperapi.dll;#1") # assembly manifest
-  execute_process(
-    COMMAND "mt.exe -manifest libpantab.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")  # application manifest
+    COMMAND "mt.exe -manifest libpantab.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;2")  # application manifest
 endif()
