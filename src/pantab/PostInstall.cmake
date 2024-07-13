5d85c14ff21cef791c2496f788077ef8f5e57d44
Will Ayd <william.ayd@icloud.com>
2024-07-13 00:46:25 -0400

    Try separate manifests

--- src/pantab/PostInstall.cmake
+++ src/pantab/PostInstall.cmake
@@ -1,5 +1,7 @@
 # https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
 if(WIN32)
   execute_process(
-    COMMAND "mt.exe -manifest libpantab.dll.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")
+    COMMAND "mt.exe -manifest libpantab.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")
+  execute_process(
+    COMMAND "mt.exe -manifest tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\tableauhyperapi.dll;#2")
 endif()
