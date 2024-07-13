19c391269f3a1b0159724742e67cc78007055d24
Will Ayd <william.ayd@icloud.com>
2024-07-13 10:08:05 -0400

    Try different resource embedding

--- src/pantab/PostInstall.cmake
+++ src/pantab/PostInstall.cmake
@@ -1,7 +1,7 @@
 # https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
 if(WIN32)
   execute_process(
-    COMMAND "mt.exe -manifest libpantab.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")
+    COMMAND "mt.exe -manifest tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#1") # assembly manifest
   execute_process(
-    COMMAND "mt.exe -manifest tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\tableauhyperapi.dll;#2")
+    COMMAND "mt.exe -manifest libpantab.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;#2")  # application manifest
 endif()
