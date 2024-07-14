19cb97c23ab731ccd9e9a490ed7b1eeea71be67b
Will Ayd <william.ayd@icloud.com>
2024-07-13 23:45:07 -0400

    remove post-install script

--- src/pantab/PostInstall.cmake
+++
@@ -1,5 +0,0 @@
-# https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
-if(WIN32)
-  execute_process(
-    COMMAND "mt.exe -manifest libpantab.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\libpantab.dll;2")  # application manifest
-endif()
