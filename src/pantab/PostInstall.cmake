454b0dc42b10590aa9ab148460f2d6dac36c00c9
William Ayd <will_ayd@innobi.io>
2024-07-19 15:53:53 -0400

    Update PostInstall.cmake

--- src/pantab/PostInstall.cmake
+++ src/pantab/PostInstall.cmake
@@ -1,5 +1,5 @@
 # https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
 if(WIN32)
   execute_process(
-    COMMAND "mt.exe -manifest tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\tableauhyperapi.dll;1")
+    COMMAND "mt.exe -manifest ${SKBUILD_PROJECT_NAME}\\tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\tableauhyperapi.dll;1")
 endif()
