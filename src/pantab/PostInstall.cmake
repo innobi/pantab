02a7b048690d39671b64150ed7c672658ad8578e
Will Ayd <william.ayd@icloud.com>
2024-07-19 15:38:17 -0400

    Try overriding tableau manifest

---
+++ src/pantab/PostInstall.cmake
@@ -0,0 +1,5 @@
+# https://learn.microsoft.com/en-us/cpp/build/understanding-manifest-generation-for-c-cpp-programs?view=msvc-170#two-approaches
+if(WIN32)
+  execute_process(
+    COMMAND "mt.exe -manifest tableauhyperapi.manifest -outputresource:${SKBUILD_PROJECT_NAME}\\tableauhyperapi.dll;1")
+endif()
