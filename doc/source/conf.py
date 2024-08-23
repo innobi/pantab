99117488c79001996f122262c154abddf43c8fe6
Will Ayd <william.ayd@icloud.com>
2024-08-22 21:02:46 -0400

    Hack to add logo to changelog

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,5 +33,4 @@ html_static_path = ["_static"]
 # -- Options for opengraph output --------------------------------------------
 
 ogp_site_url = "https://pantab.readthedocs.io/"
-ogp_use_first_image = False
 ogp_image = "https://pantab.readthedocs.io/en/latest/_static/pantab_logo.png"
